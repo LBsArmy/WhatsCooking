69474053aad4f9004d2a007dfa9539a2a5885e3b
Afischler <andres.fischler@gmail.com>
2015-11-16 17:09:17 -0500

    Updated 11.16.15
    MBA approach

--- cooking_updt.R
+++ cooking_updt.R
@@ -10,11 +10,12 @@ require(stringr)
 require(tidyr)
 require(plyr)
 require(dplyr)
+require(qdap)
 
 
 # Load Data
-#train <- "C:\\Users\\FISCA028\\Downloads\\train.json\\train.json"
-train<- train <- "C:\\Users\\AFischler\\Desktop\\Kaggle\\Cooking\\train.json"
+train <- "C:\\Users\\FISCA028\\Downloads\\train.json\\train.json"
+#train<- train <- "C:\\Users\\AFischler\\Desktop\\Kaggle\\Cooking\\train.json"
 
 train2 <- fromJSON(train, flatten=TRUE)
 
@@ -101,6 +102,62 @@ for (i in seq(1:length(LHS2))){
         }
 }
 
+#remove V#s=:
+v_string <- paste("V",1:65,"=",sep="")
+v_string2 <- paste(v_string, collapse = '|')
+
+LHS_c1 <- gsub(v_string2,"", LHS3[,1])
+LHS_c2 <- gsub(v_string2,"", LHS3[,2])
+LHS_c3 <- gsub(v_string2,"", LHS3[,3])
+
+#remove =s
+LHS_c1 <- gsub(" =","", LHS_c1)
+LHS_c2 <- gsub(" =","", LHS_c2)
+LHS_c3 <- gsub(" =","", LHS_c3)
+
+# merge LHS back together
+LHS_clean <- as.data.frame(cbind(LHS_c1,LHS_c2,LHS_c3))
+colnames(LHS_clean) <- c("Piece1","Piece2","Piece3")
+
+# Combine LHS_clean with rest of data:
+
+final_rules <- as.data.frame(cbind(try4,LHS_clean))
+attach(final_rules)
+final_rules <- data.frame(Piece1, Piece2, Piece3,cuisine,support,confidence,lift)
+detach(final_rules)
+
+
+#create small testing batch
+
+train_small <- train2[1:10,]
+
+# Create rules lists by number of ingredients
+
+one_ing <- final_rules[is.na(final_rules[,2]),]
+two_ing <- final_rules[(is.na(final_rules[,3]) & !is.na(final_rules[,2])) ,]
+three_ing <- final_rules[!is.na(final_rules[,3]),]
+
+
+
+level1 <- 0
+scores <-matrix(, nrow = nrow(train_small), ncol = length(cuisines))
+time <- Sys.time()
+cuisines <- unique(train2$cuisine)
+
+
+
+for (g in (1:nrow(train_small))){
+    for (i in (1:nrow(one_ing))){
+        level1 <- level1 + grepl(one_ing$Piece1[i],train_small$ingredients[g]) * one_ing$lift[i]
+        }
+    scores1 <- as.data.frame(rbind(scores1,level1))
+    if (g %% 1000 == 0){
+      print(paste(round(i/nrow(train_small),3),"%", round(Sys.time() - time,2), sep=" "))
+      time <- Sys.time()
+    }
+    level1 <- 0
+}
+    
 
 
 ####### WIP
