9b3f44b7129e23de3faa9806fff0daa2db7d6e17
Afischler <andres.fischler@gmail.com>
2015-11-18 16:56:19 -0500

    Updated 11.18.15
    rerun with new transaction database

--- cooking_updt.R
+++ cooking_updt.R
@@ -11,6 +11,8 @@ require(tidyr)
 require(plyr)
 require(dplyr)
 require(qdap)
+require(caret)
+require(datasets)
 
 
 # Load Data
@@ -49,12 +51,17 @@ for (i in seq(1:length(test))){
 
 df2 <- cbind(train2$cuisine, df)
 
+write.table(df2,file=paste(getwd(),"/df2.csv",sep=""), row.names=FALSE, na="", sep=",")
+
+df3 <- read.transactions(file=paste(getwd(),"/df2.csv",sep=""),format="basket", sep=",", rm.duplicates = TRUE)
+
 
 
 # Get the rules
-rules <- apriori(df2, parameter = list(supp = 0.0001, conf = 0.05, minlen =2, maxlen = 4, target="rules"))
+rules <- apriori(df3, parameter = list(supp = 0.0001, conf = 0.05, minlen =2, maxlen = 4, target="rules"))
+itemFrequencyPlot(df3, topN = 25)
 
-itemFrequencyPlot(df2, topN = 25)
+rules.sub <- subset(rules, subset = rhs %pin% "greek")
 
 # Show the top 5 rules, but only 2 digits
 options(digits=2)
@@ -139,29 +146,122 @@ three_ing <- final_rules[!is.na(final_rules[,3]),]
 
 
 
-level1 <- 0
-scores <-matrix(, nrow = nrow(train_small), ncol = length(cuisines))
-time <- Sys.time()
 cuisines <- unique(train2$cuisine)
+scores <-matrix(0, nrow = nrow(train_small), ncol = length(cuisines))
+colnames(scores) <- cuisines
+time <- Sys.time()
+
 
 
 
 for (g in (1:nrow(train_small))){
     for (i in (1:nrow(one_ing))){
-        level1 <- level1 + grepl(one_ing$Piece1[i],train_small$ingredients[g]) * one_ing$lift[i]
+        if (grepl(one_ing$Piece1[i],train_small$ingredients[g])){
+            scores[g,which( cuisines == one_ing$cuisine[i])] <- scores[g,which( cuisines == one_ing$cuisine[i])] + one_ing$lift[i]
         }
-    scores1 <- as.data.frame(rbind(scores1,level1))
+        
+    }
     if (g %% 1000 == 0){
-      print(paste(round(i/nrow(train_small),3),"%", round(Sys.time() - time,2), sep=" "))
+      print(paste(round(g/nrow(train_small),3),"%", round(Sys.time() - time,2), sep=" "))
       time <- Sys.time()
     }
-    level1 <- 0
 }
-    
+
+#Full data
+
+
+cuisines <- unique(train2$cuisine)
+scores_sm <-matrix(0, nrow = nrow(train_small), ncol = length(cuisines))
+count_sm <- matrix(0, nrow = nrow(train_small), ncol = length(cuisines))
+colnames(scores) <- cuisines
+time <- Sys.time()
+
+for (g in (1:nrow(train_small))){
+  for (i in (1:nrow(one_ing))){
+    if (grepl(one_ing$Piece1[i],train_small$ingredients[g])){
+      scores_sm[g,which(cuisines == one_ing$cuisine[i])] <- round(scores_sm[g,which(cuisines == one_ing$cuisine[i])] + (one_ing$lift[i] - 1) * one_ing$confidence[i] / one_ing$support[i] /10000,2) 
+      count_sm[g,which(cuisines == one_ing$cuisine[i])] <- count_sm[g,which(cuisines == one_ing$cuisine[i])] + 1
+    }
+  }
+}
+  if (g %% 1000 == 0){
+    print(paste(round(g/nrow(train2),3),"%", round(Sys.time() - time,2), sep=" "))
+    time <- Sys.time()
+  }
+  if (g == 5000){
+      write.csv(scores[1:5000,],file=paste(getwd(),"/scores1.csv",sep=""), row.names=FALSE)}
+  else if (g == 10000){
+    write.csv(scores[5001:10000,],file=paste(getwd(),"/scores2.csv",sep=""), row.names=FALSE)}
+  else if (g == 15000){
+    write.csv(scores[10001:15000,],file=paste(getwd(),"/scores3.csv",sep=""), row.names=FALSE)}
+  else if (g == 20000){
+    write.csv(scores[15001:20000,],file=paste(getwd(),"/scores4.csv",sep=""), row.names=FALSE)}
+  else if (g == 25000){
+    write.csv(scores[20001:25000,],file=paste(getwd(),"/scores5.csv",sep=""), row.names=FALSE)}
+  else if (g == 30000){
+    write.csv(scores[25001:30000,],file=paste(getwd(),"/scores6.csv",sep=""), row.names=FALSE)}
+  else if (g == 35000){
+    write.csv(scores[30001:35000,],file=paste(getwd(),"/scores7.csv",sep=""), row.names=FALSE)}
+  else if (g == nrow(train2)){
+    write.csv(scores[35001:nrow(train2),],file=paste(getwd(),"/scores8.csv",sep=""), row.names=FALSE)}  
+}
+
+
+
+df_scores <- as.data.frame(scores)
+
+df_scores <- unlist(colnames(df_scores)[max.col(df_scores,ties.method="random")])
+
+
+# Confusion matrix
+Observations <- train2$cuisine
+Predicted <- df_scores
+conf <- table(Predicted, Observations)
+
+
+
+#Accuracy
+mean(ifelse(Observations == Predicted,1,0))
+
+
+
+
+
+
 
 
 ####### WIP
 
+aa <- matrix(c("a","b"," c","d"), nrow = 2, ncol = 2)
+
+
+write.csv(try1,file=paste(getwd(),"/one_ing2.csv",sep=""), row.names=FALSE)
+
+##### Standardizing Scores by Numbers of Ingredients:
+
+ing_count <- str_count(train2$ingredients, " ") + 1
+
+scores2 <- scores_sm 
+
+#/ count_sm
+
+df_scores2 <- as.data.frame(scores2)
+
+df_scores2 <- unlist(colnames(df_scores2)[max.col(df_scores2,ties.method="random")])
+
+
+# Confusion matrix
+Observations <- train_small$cuisine
+Predicted <- df_scores2
+conf <- table(Predicted, Observations)
+
+
+
+#Accuracy
+mean(ifelse(Observations == Predicted,1,0))
+
+
+(train_small$cuisine[g] == one_ing$cuisine[i])
 
 train2$ingredients <- gsub("[^A-Za-z0-9,_]", "", train2$ingredients)
 train2$ingredients <- gsub(",_", " ", train2$ingredients)
